5e57c414ecbe43bd23100b4803a88f68c3c2da10
galang48 <galangyudha1945@gmail.com>
2025-10-31 09:17:35 +0700

    Menambahkan state textAlamat untuk input alamat pengguna

--- app/src/main/java/com/example/questuserinput/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput/TeksEdit.kt
@@ -14,4 +14,6 @@ fun FormDataDiri(modifier: Modifier
     //variabel-variabel untuk mengingat nilai masukkan dari keyboard
 
     var textNama by remember {mutableStateOf("") }
+
+    var textAlamat by remember {mutableStateOf("") }
 }
