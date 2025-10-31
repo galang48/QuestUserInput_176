1c2e1f73a94e4f0b82b87a174e3e263864f11c82
galang48 <galangyudha1945@gmail.com>
2025-10-31 09:16:22 +0700

    Menambahkan composable FormDataDiri dengan state textNama

--- app/src/main/java/com/example/questuserinput/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput/TeksEdit.kt
@@ -1,11 +1,17 @@
 package com.example.questuserinput
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 
-fun FormDataDiri(Modifier: Modifier
+fun FormDataDiri(modifier: Modifier
 ){
+    //variabel-variabel untuk mengingat nilai masukkan dari keyboard
 
+    var textNama by remember {mutableStateOf("") }
 }
